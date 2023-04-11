b6d8fda31a5aa08cde259803e55faf24a7c38218
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-11 08:53:04 -0300

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vel autem praesentium quis vero suscipit blanditiis nesciunt a pariatur corrupti ratione exercitationem reiciendis, laudantium quo commodi ipsam veritatis, consectetur quae.</p>
+    </div>
+  )
+}
