d363e904c9482711f85367833238d0c208a12922
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-12 09:12:15 -0300

    error boundary created

---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }: { error: Error, reset: () => void }) {
+
+  useEffect(() => {
+    console.log(error)
+  }, [error])
+
+  return (
+    <div className="text-center mt-10">
+      <p>Something went wrong</p>
+      <button className="hover:text-amber-600" type="button" title="reset" onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
